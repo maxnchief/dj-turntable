d9db1db301a2e7b63725171afedc49d8404c811e
maxnchief <logandogan@gmail.com>
2025-04-19 17:41:58 -0500

    Refactor YouTube API initialization and add comments for clarity

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let player
 
+let player
+/**
+ * Callback function triggered when the YouTube IFrame API is ready.
+ * Test if the YouTube API has loaded successfully.
+ */
 function onYouTubeIframeAPIReady () {
     console.log("api is loaded")
 
@@ -7,4 +11,24 @@ function onYouTubeIframeAPIReady () {
         height: 200,
         width: 200,
         videoId: 'M7lc1UVf-VE',
-})}
+        playerVars: {
+        playersinline: 1,
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    })
+}
+
+function onPlayerReady (){
+    console.log("player is ready")
+}
+
+var done = false
+
+function onPlayerStateChange (event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        done = true
+    }
+}
